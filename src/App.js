c8ea1de21ff9891cbf0738bfa1f9491e3795269f
jm-avila <jm.avila1309@icloud.com>
2020-09-14 11:55:23 +0200

    Basic Counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function increaseCounter() {
+    setCounter((prevState) => ++prevState);
+  }
+
+  function decreaseCounter() {
+    setCounter((prevState) => --prevState);
+  }
+
+  function resetCounter() {
+    setCounter(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card border-0 mt-5 mx-auto">
+      <div className="mx-auto">
+        <h1 className="text-capitalize">counter</h1>
+      </div>
+
+      <div className="mx-auto my-3">
+        <h3>{counter}</h3>
+      </div>
+
+      <div className="mx-auto">
+        <button className="btn btn-outline-dark" onClick={decreaseCounter}>
+          decrease
+        </button>
+        <button className="btn btn-outline-dark mx-2" onClick={resetCounter}>
+          reset
+        </button>
+        <button className="btn btn-outline-dark" onClick={increaseCounter}>
+          increase
+        </button>
+      </div>
     </div>
   );
 }
