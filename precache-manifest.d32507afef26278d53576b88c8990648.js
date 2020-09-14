44514d3204fb79cd89d8d99585f04a6a01c8dd02
jm-avila <jm.avila1309@icloud.com>
2020-09-14 11:59:33 +0200

    Updates

---
+++ precache-manifest.d32507afef26278d53576b88c8990648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a16b4ffa376fa7a2a97e0d575b3533e7",
+    "url": "/react-counter/index.html"
+  },
+  {
+    "revision": "20244f3a99b210d9d440",
+    "url": "/react-counter/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "20244f3a99b210d9d440",
+    "url": "/react-counter/static/js/2.37672d60.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-counter/static/js/2.37672d60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5873a51c69424c44ba7",
+    "url": "/react-counter/static/js/main.ae545055.chunk.js"
+  },
+  {
+    "revision": "97792ca62b6f2b98f699",
+    "url": "/react-counter/static/js/runtime-main.191adfc0.js"
+  }
+]);
